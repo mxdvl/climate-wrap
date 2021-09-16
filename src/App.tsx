8270e8b154bed97fc469968f21e4d2a231b7d80a
Oliver Barnwell <barnwelloliver@gmail.com>
2021-09-16 13:26:18 +0100

    feat: basic routing£

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,48 @@
-import "./App.css";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Button, ChakraProvider, Stack } from "@chakra-ui/react";
+import { Link, Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import { Overview } from "./sections/Overview";
 import { Travel } from "./sections/Travel";
-// 1. import `ChakraProvider` component
-
-function App() {
+const App = () => {
 	return (
 		<ChakraProvider>
-			<div className="App">
-				<header className="App-header">HEADER HERE</header>
-
-				<Overview />
-
-				<Travel />
-
-				{/* etc */}
-			</div>
+			<Router>
+				<div className="App">
+					<header className="App-header">HEADER HERE</header>
+					<nav>
+						<Stack as="ul" direction="row" listStyleType="none">
+							<li>
+								<Button as={Link} to="/">
+									Home
+								</Button>
+							</li>
+							<li>
+								<Button as={Link} to="/overview">
+									Overview
+								</Button>
+							</li>
+							<li>
+								<Button as={Link} to="/travel">
+									Travel
+								</Button>
+							</li>
+						</Stack>
+					</nav>
+					<hr />
+					BODY CONTENT
+					<div>
+						<Switch>
+							<Route path="/overview">
+								<Overview />
+							</Route>
+							<Route path="travel">
+								<Travel />
+							</Route>
+						</Switch>
+					</div>
+				</div>
+			</Router>
 		</ChakraProvider>
 	);
-}
+};
 
 export default App;
