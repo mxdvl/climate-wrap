5feaf016df1ee7121703742ffed9b3b386e3130b
Oliver Barnwell <barnwelloliver@gmail.com>
2021-09-16 16:24:46 +0100

    feat: stepper

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,66 @@
-import { Button, ChakraProvider, Stack } from '@chakra-ui/react';
+import {
+	Box,
+	Button,
+	ChakraProvider,
+	extendTheme,
+	Flex,
+	HStack,
+	Stack,
+	VStack,
+} from '@chakra-ui/react';
+import { Container } from '@guardian/src-layout';
+import { Step, Steps, StepsStyleConfig, useSteps } from 'chakra-ui-steps';
 import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Overview } from './sections/Overview';
+import { Social } from './sections/Social';
+import { Spending } from './sections/Spending';
 import { Travel } from './sections/Travel';
 
+const theme = extendTheme({
+	components: {
+		Steps: StepsStyleConfig,
+	},
+});
+
 export const App = (): JSX.Element => {
+	const { nextStep, prevStep, setStep, reset, activeStep } = useSteps({
+		initialStep: 0,
+	});
+
+	const steps = [
+		{ label: 'Social', content: Social },
+		{ label: 'Spending', content: Spending },
+		{ label: 'Travel', content: Travel },
+		{ label: 'Overview', content: Overview },
+	];
+
+	const backDisabled = activeStep === 0;
+	const nextDisabled = activeStep === steps.length;
 	return (
-		<ChakraProvider>
+		<ChakraProvider theme={theme}>
 			<Router>
 				<div className="App">
-					<Header />
-					<nav>
-						<Stack as="ul" direction="row" listStyleType="none">
-							<li>
-								<Button as={Link} to="/">
-									Home
-								</Button>
-							</li>
-							<li>
-								<Button as={Link} to="/overview">
-									Overview
-								</Button>
-							</li>
-							<li>
-								<Button as={Link} to="/travel">
-									Travel
-								</Button>
-							</li>
-						</Stack>
-					</nav>
-					<hr />
-					BODY CONTENT
-					<div>
-						<Switch>
-							<Route path="/overview">
-								<Overview />
-							</Route>
-							<Route path="travel">
-								<Travel />
-							</Route>
-						</Switch>
-					</div>
+					<Stack direction="column" spacing="8">
+						<Header />
+						<Container>
+							<Steps activeStep={activeStep}>
+								{steps.map(({ label, content }) => (
+									<Step label={label} key={label}>
+										{content}
+									</Step>
+								))}
+							</Steps>
+						</Container>
+						<HStack>
+							<Button onClick={prevStep} disabled={backDisabled}>
+								Back
+							</Button>
+							<Button onClick={nextStep} disabled={nextDisabled}>
+								Next
+							</Button>
+						</HStack>
+					</Stack>
 				</div>
 			</Router>
 		</ChakraProvider>
--- src/sections/Overview.tsx
+++ src/sections/Overview.tsx
@@ -1,12 +1,5 @@
-const sections = ['Travel', 'Community', 'Home', 'Spending', 'Social'];
-
 export const Overview = (): JSX.Element => (
 	<div>
 		<h3>OVERVIEW section</h3>
-		<ul>
-			{sections.map((section) => (
-				<li>{section}</li>
-			))}
-		</ul>
 	</div>
 );
