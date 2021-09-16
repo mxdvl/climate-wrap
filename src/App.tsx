33d03647dd432529ca7da547854a66701e06307a
Max Duval <max.duval@guardian.co.uk>
2021-09-16 11:40:16 +0100

    use typescript

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import { Overview } from './sections/Overview';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        HEADER HERE
+      </header>
+      
+      <Overview />
+    </div>
+  );
+}
+
+export default App;
