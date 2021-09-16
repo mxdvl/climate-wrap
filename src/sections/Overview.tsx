1a2e28b68c66e122d11b72e1e60c0d9929824a6e
Max Duval <max.duval@guardian.co.uk>
2021-09-16 12:53:25 +0100

    add chakra-ui

--- src/sections/Overview.tsx
+++ src/sections/Overview.tsx
@@ -1,5 +1,10 @@
-export const Overview = ({}) => (
+const sections = ['Travel', 'Community', 'Home', 'Spending', 'Social'];
+
+export const Overview = () => (
 	<div>
 		<h3>OVERVIEW section</h3>
+		<ul>
+			{sections.map(section => <li>{section}</li>)}
+		</ul>
 	</div>
 );
