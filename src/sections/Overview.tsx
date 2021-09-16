94cadfa7a0018e1b4320b111f8f83eba2b173809
Max Duval <max.duval@guardian.co.uk>
2021-09-16 16:14:22 +0100

    Make overview work globally

--- src/sections/Overview.tsx
+++ src/sections/Overview.tsx
@@ -9,6 +9,7 @@ import {
 	Wrap,
 	WrapItem,
 } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
 
 type Section = {
 	name: string;
@@ -52,65 +53,137 @@ const sections: Record<SectionType, Section> = {
 	},
 };
 
-const consumptions: Record<SectionType, number> = {
+type Consumptions = Record<SectionType, number>;
+
+const defaultConsumptions: Consumptions = {
 	travel: 600,
-	home: 2000,
+	home: 1800,
 	spending: 500,
 	social: 1200,
 	community: 0,
 };
 
-export const Overview = (): JSX.Element => (
-	<Box bg={theme.colors.gray[100]}>
-		<Heading as="h3" textAlign="center">
-			OVERVIEW section
-		</Heading>
+const getTotal = (consumptions: Consumptions) => {
+	const real = Object.values(consumptions).reduce(
+		(total, add) => total + add,
+	);
+	const values = Object.values(consumptions)
+		.filter((v) => v > 0)
+		.sort(numeric);
+	const [one, two, three, four] = values;
+	const visual = Math.max(one + four, two + three) * 2;
 
-		<Wrap spacing={2} align="center" justify="center" width="100%">
-			{Object.entries(sections).map((entry) => {
-				const [id, section] = entry as [SectionType, Section];
-				const total = Object.values(consumptions).reduce(
-					(total, add) => total + add,
-				);
-				const consumption = consumptions[id];
-				const width = `${(180 * consumption) / total}vw`;
-				const height = '40vh';
-				const order =
-					Object.values(consumptions)
-						.sort((a, b) => b - a)
-						.indexOf(consumption) + 1;
+	return Math.max(real, visual);
+};
 
-				if (id == 'community') return;
+const numeric = (a: number, b: number) => b - a;
 
-				return (
-					<WrapItem key={id} order={order}>
-						<Box
-							padding={1}
-							margin={1}
-							width={width}
-							height={height}
-							background={section.colour}
-							flexWrap="wrap"
-						>
-							<Heading as="h4">{section.name}</Heading>
-							{section.description}
-						</Box>
-					</WrapItem>
-				);
-			})}
-		</Wrap>
+export const Overview = (): JSX.Element => {
+	const [consumptions, setConsumptions] = useState(defaultConsumptions);
 
-		<SliderController></SliderController>
-	</Box>
-);
+	const total = getTotal(consumptions);
 
-const SliderController = () => {
 	return (
-		<Slider defaultValue={100}>
-			<SliderTrack>
-				<SliderFilledTrack />
-			</SliderTrack>
-			<SliderThumb />
-		</Slider>
+		<Box bg={theme.colors.gray[100]}>
+			<Heading as="h3" textAlign="center">
+				OVERVIEW section
+			</Heading>
+
+			<Wrap spacing={2} align="center" justify="center" width="100%">
+				{Object.entries(sections).map((entry) => {
+					const [id, section] = entry as [SectionType, Section];
+					const consumption = consumptions[id];
+					const width = `${1.8 * 100 * (consumption / total)}vw`;
+					const height = '40vh';
+					const order =
+						Object.values(consumptions)
+							.filter((v) => v > 0)
+							.sort(numeric)
+							.indexOf(consumption) + 1;
+
+					if (id == 'community') return;
+
+					return (
+						<WrapItem key={id} order={order === 1 ? -1 : order % 4}>
+							<Box
+								padding={1}
+								margin={1}
+								width={width}
+								height={height}
+								background={section.colour}
+								flexWrap="wrap"
+							>
+								<Heading as="h4">{section.name}</Heading>
+								{section.description}
+
+								<Heading as="h5">
+									{consumption} Kg CO<sub>2</sub>
+								</Heading>
+							</Box>
+						</WrapItem>
+					);
+				})}
+			</Wrap>
+
+			<Slider
+				value={consumptions.travel}
+				min={1}
+				max={2000}
+				onChange={(value) => {
+					setConsumptions({ ...consumptions, travel: value });
+					return value;
+				}}
+			>
+				<SliderTrack>
+					<SliderFilledTrack bg={sections.travel.colour} />
+				</SliderTrack>
+				<SliderThumb />
+			</Slider>
+
+			<Slider
+				value={consumptions.home}
+				min={1}
+				max={2000}
+				onChange={(value) => {
+					setConsumptions({ ...consumptions, home: value });
+					return value;
+				}}
+			>
+				<SliderTrack>
+					<SliderFilledTrack bg={sections.home.colour} />
+				</SliderTrack>
+				<SliderThumb />
+			</Slider>
+
+			<Slider
+				defaultValue={consumptions.spending}
+				min={1}
+				max={2000}
+				onChange={(value) => {
+					setConsumptions({ ...consumptions, spending: value });
+					return value;
+				}}
+			>
+				<SliderTrack>
+					<SliderFilledTrack bg={sections.spending.colour} />
+				</SliderTrack>
+				<SliderThumb />
+			</Slider>
+
+			<Slider
+				value={consumptions.social}
+				min={1}
+				max={2000}
+				onChange={(value) => {
+					setConsumptions({ ...consumptions, social: value });
+					return value;
+				}}
+			>
+				<SliderTrack>
+					<SliderFilledTrack bg={sections.social.colour} />
+				</SliderTrack>
+				<SliderThumb />
+			</Slider>
+		</Box>
 	);
 };
