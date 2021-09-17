0f08605a84e9bdbbf59028deab2a8eb2ce3c14a2
Oliver Barnwell <barnwelloliver@gmail.com>
2021-09-17 15:06:03 +0100

    feat chart on supplier

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -1,5 +1,6 @@
 import {
 	Box,
+	Center,
 	Divider,
 	Heading,
 	HStack,
@@ -9,6 +10,7 @@ import {
 	Text,
 } from '@chakra-ui/react';
 import React, { ChangeEventHandler } from 'react';
+import { Gauge } from '../components/Gauge';
 import type { Supplier, SupplierFuelMix } from '../hooks/climateWrapped';
 import {
 	useCarbonIntensity,
@@ -45,7 +47,9 @@ const SupplierInfoPanel: React.FC<SupplierInfoPanelProps> = ({
 				const k = key.charAt(0).toUpperCase() + key.slice(1);
 				const v = fuelMix && fuelMix[key as keyof SupplierFuelMix];
 				const percentFiller =
-					key === 'year' || key === 'nuclear waste' ? '' : '%';
+					key === 'year' || key === 'nuclear waste' || key === 'CO2'
+						? ''
+						: '%';
 				return (
 					<>
 						<HStack>
@@ -77,6 +81,18 @@ export const Home = (): JSX.Element => {
 		supplier?.code ?? '',
 	);
 
+	const { suppliers: ALLFMIX } = useSuppliersFuelMix();
+
+	const average =
+		(ALLFMIX ?? []).reduce((total, next) => total + next.CO2, 0) /
+		(ALLFMIX ?? []).length;
+
+	const maxAll = Math.max(
+		...(ALLFMIX ?? []).map(function (o) {
+			return o.CO2;
+		}),
+	);
+
 	const onChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
 		setState({
 			...state,
@@ -93,36 +109,45 @@ export const Home = (): JSX.Element => {
 				<Heading as="h2" size="3xl" textAlign="center">
 					Home
 				</Heading>
-				<Stack spacing="8" w="50%">
-					<Stack spacing="4">
-						<Heading as="h3" size="lg">
-							Find your supplier
-						</Heading>
-						<Divider />
-						<Select
-							placeholder="Select option"
-							value={home.selectedSupplier}
-							onChange={onChange}
-						>
-							{suppliers?.map((supplier) => (
-								<option
-									key={supplier.code}
-									value={supplier.code}
-								>
-									{supplier.name}
-								</option>
-							))}
-						</Select>
+				<HStack>
+					<Stack spacing="8" w="50%">
+						<Stack spacing="4">
+							<Heading as="h3" size="lg">
+								Find your supplier
+							</Heading>
+							<Divider />
+							<Select
+								placeholder="Select option"
+								value={home.selectedSupplier}
+								onChange={onChange}
+							>
+								{suppliers?.map((supplier) => (
+									<option
+										key={supplier.code}
+										value={supplier.code}
+									>
+										{supplier.name}
+									</option>
+								))}
+							</Select>
+						</Stack>
+						<Box>
+							{supplier && (
+								<SupplierInfoPanel
+									supplier={supplier}
+									fuelMix={supplierFuelMix}
+								/>
+							)}
+						</Box>
 					</Stack>
-					<Box>
-						{supplier && (
-							<SupplierInfoPanel
-								supplier={supplier}
-								fuelMix={supplierFuelMix}
-							/>
-						)}
-					</Box>
-				</Stack>
+					<Center w="50%">
+						<Gauge
+							score={(supplierFuelMix?.CO2 ?? 0) / maxAll}
+							title="How does your supplier compare to the average CO2 emissions?"
+							average={average}
+						/>
+					</Center>
+				</HStack>
 			</Box>
 		</Stack>
 	);
