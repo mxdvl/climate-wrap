e16c47711e618eda7596294b1a1966f5cf873113
Rebecca Thompson <becca.al.thompson@gmail.com>
2021-09-17 14:05:13 +0100

    travel page

--- src/State.tsx
+++ src/State.tsx
@@ -4,6 +4,8 @@ import type { CarbonItem } from './components/CarbonItems';
 interface ApplicationState {
 	travel: {
 		flights: number;
+		cars: number;
+		commute: number;
 	};
 	carbonItems: CarbonItem[];
 }
@@ -16,16 +18,29 @@ interface Store {
 export const carbonPerKm = {
 	flight: 0.22, // kg CO2 / km
 	car: 0.19, // kg CO2 / km
+	commute: 0.02, // MADE UP VALUE!!
 };
 
 export const flightValues = {
-	maxKms: 50000,
-	avgKms: 11000,
+	maxKms: 50_000,
+	avgKms: 11_000,
+};
+
+export const carValues = {
+	maxKms: 6_000,
+	avgKms: 4_000,
+};
+
+export const commuteValues = {
+	maxKms: 12_000,
+	avgKms: 6_000,
 };
 
 export const initState: ApplicationState = {
 	travel: {
 		flights: flightValues.avgKms * carbonPerKm.flight,
+		cars: carValues.avgKms * carbonPerKm.car,
+		commute: commuteValues.avgKms * carbonPerKm.commute,
 	},
 	carbonItems: [],
 };
