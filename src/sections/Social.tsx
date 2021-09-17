b7252e2f29c8c37ba938721cc7e55aafe5321e41
Oliver Barnwell <barnwelloliver@gmail.com>
2021-09-17 12:36:55 +0100

    wip

--- src/sections/Social.tsx
+++ src/sections/Social.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Stack } from '@chakra-ui/react';
+import { Box, Heading, Radio, RadioGroup, Stack } from '@chakra-ui/react';
 import { sections } from './Sections';
 
 export const Social = (): JSX.Element => {
@@ -9,6 +9,15 @@ export const Social = (): JSX.Element => {
 				<Heading as="h2" size="3xl" textAlign="center">
 					Social
 				</Heading>
+				<Heading as="h3" size="lg">
+					Diet
+				</Heading>
+				<RadioGroup>
+					<Radio value="1">Meat</Radio>
+					<Radio value="2">Vegitarian</Radio>
+					<Radio value="3">Pescitarian</Radio>
+					<Radio value="4">Vegan</Radio>
+				</RadioGroup>
 			</Box>
 		</Stack>
 	);
